e2e4694330792aa5139e9fa6daa730c4d3eccd12
Hetan Nandre <97376783+nandrehetan@users.noreply.github.com>
2023-05-23 18:55:00 +0530

    Create Prob2_Part2.java

---
+++ Lab Exam/Prob2_Part2.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Prob2_Part2 {
+    public static void main(String[] args){
+        String s = "umbrella";
+        if(s.contains("e")){
+            System.out.println("Given string contains 'e' character");
+        }
+        else{
+            System.out.println("Given string does not contain 'e' character");
+        }
+    }
+}
